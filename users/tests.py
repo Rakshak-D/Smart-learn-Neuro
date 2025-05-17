d42fc4f72896167df09d59ef27073baeb9fb5d42
Rakshak D <rakshakmce@gmail.com>
2025-05-17 18:38:44 +0530

    Add unit tests for CustomUser model and settings views

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import CustomUser
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='testuser', password='testpass', font_size=18, prefers_audio=True
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.font_size, 18)
+        self.assertTrue(self.user.prefers_audio)
+
+    def test_login_view(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpass'
+        })
+        self.assertEqual(response.status_code, 302)  # Redirect to profile
