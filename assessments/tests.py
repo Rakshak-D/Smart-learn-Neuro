4b38bf7552a477d6c294bc4a7c219c4a80dec28e
Rakshak D <rakshakmce@gmail.com>
2025-05-17 18:38:39 +0530

    Add unit tests for assessment models and views

--- assessments/tests.py
+++ assessments/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Assessment, Response
 
-# Create your tests here.
+class AssessmentTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        self.assessment = Assessment.objects.create(title='Test Assessment')
+
+    def test_create_response(self):
+        response = Response.objects.create(
+            user=self.user, assessment=self.assessment, response_text='Test response'
+        )
+        self.assertEqual(response.response_text, 'Test response')
+        self.assertEqual(str(response), f"{self.user.username} - {self.assessment.title}")
